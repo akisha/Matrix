776283d7bce2315a0da04f845436192c11e007b4
akisha <akiliza@yandex.ru>
2016-10-01 00:38:14 +0300

    3.0

--- Matrix.java
+++ Matrix.java
@@ -2,30 +2,51 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Matrix {
-    public Matrix() {
+    public double[][] matrix;
+    public static int CountRow() {
+        int m = 0;
         try {
             FileReader fs = new FileReader("C:\\Users\\akili\\IdeaProjects\\Matrix\\Test.txt");
             BufferedReader bfs = new BufferedReader(fs);
-            int n = 0, m = 1;
-            Scanner sc = new Scanner(bfs.readLine());
-            while (sc.hasNextDouble()) {
-                sc.next();
-                n++;
-            }
-            while (bfs.readLine() != null){
+            while (bfs.readLine() != null) {
                 m++;
             }
-            System.out.println(m);
-            System.out.println(n);
             fs.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return m;
+    }
+    public static int CountCol() {
+        int n = 0;
+        FileReader fs = null;
+        try {
+            fs = new FileReader("C:\\Users\\akili\\IdeaProjects\\Matrix\\Test.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader bfs = new BufferedReader(fs);
+        try {
+            Scanner sc = new Scanner(bfs.readLine());
+            while (sc.hasNextDouble()){
+                n++;
+                sc.nextDouble();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return n;
+    }
+    public static void InMatr(){
+
     }
     public static void main(String[] args) {
-        Matrix matr = new Matrix();
+        int m = CountRow();
+        int n = CountCol();
+        System.out.println(m);
+        System.out.println(n);
     }
 }
 
